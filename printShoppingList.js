b7e62282275552b9d6142801430638f82dd57b91
Vaztonyo <tony.dewet7@gmail.com>
2016-11-21 15:06:47 +0200

    corrections made on printShoppingList

--- printShoppingList.js
+++ printShoppingList.js
@@ -1,12 +1,12 @@
-var printShoppingList = [
-  { itemName : 'Bread', price : 11.00},
-  { itemName : 'Milk', price : 7.00},
-  { itemName : 'Bread', price : 19.50}
+function printShoppingList(list){
+  for (i in list){
+    console.log(list[i].itemName + ' @ ' + 'R' + list[i].price);
+  }
+}
+
+var shoppingList = [
+    { itemName : 'Bread', price : 11.00 },
+    { itemName : 'Milk', price : 7.00 },
+    { itemName : 'Cheese', price : 19.50 }
 ];
-
-console.log('Shopping List:');
-
-for (var i = 0; i < printShoppingList.length; i++){
-  var listItem = printShoppingList[i];
-  console.log(listItem.itemName + " @ " + "R" + listItem.price);
-};
+printShoppingList(shoppingList);
