6b0862f5bd40c3ed0b321f3811d561d61c58995b
Vaztonyo <tony.dewet7@gmail.com>
2016-11-22 13:50:09 +0200

    found the department which is most profitable in the sales data

---
+++ mostProfitableDepartment.js
@@ -0,0 +1,32 @@
+function mostProfitableDepartment(data){
+  var NumOfSales = 0;
+  var depart = [];
+
+  for (i in data){
+   if(data[i].sales > NumOfSales){
+     NumOfSales = data[i].sales;
+     depart = data[i];
+   }
+ }
+ return depart.department;
+}
+
+var salesData = [
+    {department : 'hardware', sales : 4500, day : 'Monday'},
+    {department : 'outdoor', sales : 1500, day : 'Monday'},
+    {department : 'carpentry', sales : 5500, day : 'Monday'},
+    {department : 'hardware', sales : 7500, day : 'Tuesday'},
+    {department : 'outdoor', sales : 2505, day : 'Tuesday'},
+    {department : 'carpentry', sales : 1540, day : 'Tuesday'},
+    {department : 'hardware', sales : 1500, day : 'Wednesday'},
+    {department : 'outdoor', sales : 8507, day : 'Wednesday'},
+    {department : 'carpentry', sales : 8009, day : 'Wednesday'},
+    {department : 'hardware', sales : 12000, day : 'Thursday'},
+    {department : 'outdoor', sales : 18007, day : 'Thursday'},
+    {department : 'carpentry', sales : 6109, day : 'Thursday'},
+    {department : 'hardware', sales : 7005, day : 'Friday'},
+    {department : 'outdoor', sales : 12006, day : 'Friday'},
+    {department : 'carpentry', sales : 16109, day : 'Friday'},
+];
+
+console.log(mostProfitableDepartment(salesData));
